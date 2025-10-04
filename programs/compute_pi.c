1878221b720882b11edff9e8d8a82d5bb8c9a6f5
Adam Baumgartner <adambaumgartner@gmail.com>
2025-10-04 04:21:56 +0000

    adding compute_pi.c

---
+++ programs/compute_pi.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    long long int n, i;
+    double pi = 0.0;
+    int sign = 1;
+
+    // Number of iterations
+    printf("Enter the number of iterations: ");
+    scanf("%lld", &n);
+
+    for (i = 0; i < n; i++) {
+        pi += sign * 4.0 / (2 * i + 1);
+        sign = -sign;  // Alternate the sign
+    }
+
+    printf("Computed value of Pi: %.15f\n", pi);
+    return 0;
+}
